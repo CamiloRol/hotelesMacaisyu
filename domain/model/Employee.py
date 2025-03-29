cf0b80769c8254e7cb8fcd11161aee9265728562
DESARROLLO <desarrollo@montolivo.com>
2025-03-29 10:55:22 -0500

    basing

--- domain/model/Employee.py
+++ domain/model/Employee.py
@@ -2,5 +2,8 @@ from domain.model.User import User
 
 
 class Employee(User):
-    def __init__(self, id, name, last_name, email, password, status):
+    def __init__(self, id, name, last_name, email, password, status, salary, address):
         super.__init__(id, name, last_name, email, password, status)
+        self.salary = salary
+        self.address = address
+
