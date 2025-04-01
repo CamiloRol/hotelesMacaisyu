d68b144e6017a269055a8417b3c16eeb79b8d67e
Manuela <manuelamunozlujan0@gmail.com>
2025-04-01 10:33:49 -0500

    Class Room added. Some changes in User and Guest (__str__ method)

--- domain/model/Guest.py
+++ domain/model/Guest.py
@@ -3,7 +3,19 @@ from domain.model.User import User
 
 class Guest(User):
 
-    def __init__(self, id, name, last_name, email, password, status, origin, occupation):
+    def __init__(self, id, name, last_name, email, password, status, origin, occupation, check_in, check_out):
         super.__init__(id, name, last_name, email, password, status)
         self.origin = origin
         self.occupation = occupation
+        self.check_in = check_in
+        self.check_out = check_out
+
+    def room_book(self, room):
+        if room.available:
+            room.mark_available(False)
+            return f"Habitación {room.room_number} reservada correctamente a {self._name}"
+        else:
+            return f"Habitación {room.room_number} no se encuentra disponible"
+
+    def __str__(self):
+        return f"Húesped: {self._name} {self._last_name}, Origen: {self.origin}, Ocupacion {self.occupation}, Fecha entrada: {self.check_in}, Fecha salida: {self.check_out}"
--- domain/model/User.py
+++ domain/model/User.py
@@ -8,7 +8,6 @@ class User:
         self._password = password
         self._status = status
 
-
     @property
     def get_id(self):
         return self._id
@@ -56,3 +55,6 @@ class User:
     @get_status.setter
     def set_status(self, status):
         self._status = status
+
+    def __str__(self):
+        return f"User: {self._name} {self._last_name}, Email: {self._email}, Status: {self._status}"
