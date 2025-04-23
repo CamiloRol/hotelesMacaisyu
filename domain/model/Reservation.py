b317cf027181d249d4ae189f51dd694c6b05c318
Israel <cuadrosisrael947@gmail.com>
2025-04-23 11:10:37 -0500

    se agregan los nuevos parametros basados en la BD para reservation y se deja funcional la clase

--- domain/model/Reservation.py
+++ domain/model/Reservation.py
@@ -1,14 +1,26 @@
 class Reservation:
-
-    def __init__(self, guest_id, room_number, booking_date):
-        self.guest_id = guest_id
+    def __init__(self, id, id_guest, id_employee, room_number, id_service,
+                 booking_date, start_date, end_date, estado='confirmada'):
+        self.id = id
+        self.id_guest = id_guest
+        self.id_employee = id_employee
         self.room_number = room_number
+        self.id_service = id_service
         self.booking_date = booking_date
+        self.start_date = start_date
+        self.end_date = end_date
+        self.estado = estado
 
     def __str__(self):
         return (
             f"Reservation Details:\n"
-            f"  - Guest ID     : {self.guest_id}\n"
+            f"  - ID           : {self.id}\n"
+            f"  - Guest ID     : {self.id_guest}\n"
+            f"  - Employee ID  : {self.id_employee}\n"
             f"  - Room Number  : {self.room_number}\n"
-            f"  - Booking Date : {self.booking_date}"
+            f"  - Service ID   : {self.id_service}\n"
+            f"  - Booking Date : {self.booking_date}\n"
+            f"  - Start Date   : {self.start_date}\n"
+            f"  - End Date     : {self.end_date}\n"
+            f"  - Status       : {self.estado}"
         )
