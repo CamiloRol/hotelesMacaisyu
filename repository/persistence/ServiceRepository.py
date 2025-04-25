60db864b1bacbbca89193379c9607f8901eccf64
Manuela <manuelamunozlujan0@gmail.com>
2025-04-24 20:27:06 -0500

    Actualización de ServiceRepository

--- repository/persistence/ServiceRepository.py
+++ repository/persistence/ServiceRepository.py
@@ -6,7 +6,23 @@ class ServiceRepository:
         self.service = Services
 
     def create_service_repository(self, service, db):
-
         query = "INSERT INTO Services (id_service, description, price) VALUES (%s, %s, %s)"
         values = (service.id_service, service.description, service.price)
         db.execute_query(query, values)
+
+    def find_all_services(self, db):
+            query = "SELECT * FROM Services"
+            response = db.execute_query(query)
+            return response
+
+    def update_service(self, service, db):
+        query = "UPDATE Services SET description = %s, price = %s  WHERE id_service = %s"
+        values = (service.description, service.price, service.id_service)
+        response = db.execute_query(query, values)
+        return response
+
+    def delete_service(self, id_service, db):
+        query = "DELETE FROM Services WHERE id_serive = %s"
+        values = (id_service,)
+        response = db.execute_query(query,values)
+        return response
